74dedc7da5423d4c1aa5829acfa748f68cad0b8f
JuliaMaxy <jjuliamaxxx@gmail.com>
2023-09-01 14:08:05 +0300

    feat: allow the user to chose color
    Change the background-color of squares according to the chosen color
    
    Style inputs inside the navigation bars

--- sketch.js
+++ sketch.js
@@ -1,26 +1,33 @@
 const container = document.getElementById('grid-container');
-const sizeRange = document.getElementById('size-input');
-function setGrid(size){
-    container.innerHTML = '';
-    console.log(size);
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i=0; i<size*size; i++) {
-        const newDiv = document.createElement('div');
-        newDiv.className = 'square';
-        container.append(newDiv);
-    }
+const sizeInput = document.getElementById('size-input');
+const colorInput = document.getElementById('color-input');
+function changeColor(color) {
     const squares = document.querySelectorAll('.square');
     
     squares.forEach((square) => {
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'black';
+            square.style.backgroundColor = color;
         })
     })
+}
+function setGrid(size){
+    container.innerHTML = '';
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size*size; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.className = 'square';
+        container.append(newDiv);
+    }
+    changeColor(colorInput.value);
 }
 
 setGrid(16);
 
-sizeRange.addEventListener('change', () => {
-     setGrid(sizeRange.value); 
+sizeInput.addEventListener('change', () => {
+     setGrid(sizeInput.value); 
+})
+
+colorInput.addEventListener('change', () => {
+    changeColor(colorInput.value);
 })
