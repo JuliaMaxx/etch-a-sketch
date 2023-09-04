d567c2f2a15d7b99ac65ba72544cea8d8d28e2a5
JuliaMaxy <jjuliamaxxx@gmail.com>
2023-09-04 11:59:33 +0300

    feat: add preloader to the page
    Remove the preloader only once all the images are loaded
    
    Put preloader js into a separate js file that is not deferred
    
    Copy css for preloader from codepan.io

---
+++ loader.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('img');
+    const preloader = document.querySelector('.loader');
+    let imageCount = 0;
+
+    function checkImagesLoaded() {
+        imageCount++;
+        if (imageCount === images.length) {
+            preloader.style.display = 'none';
+        }
+    }
+
+    images.forEach(image => {
+        image.addEventListener('load', checkImagesLoaded);
+    });
+});
