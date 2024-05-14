1fcd05de51c378bb9cca93774b0aa1a2019f2d2c
Julia Max <jjuliamaxxx@gmail.com>
2024-05-14 22:28:10 +0200

    refactor: make sure website loads faster
    Change some images to plain color
    Chnage the loader to load for 5 minutes

--- loader.js
+++ loader.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const images = document.querySelectorAll("img");
   const preloader = document.querySelector(".loader");
-  let imageCount = 0;
 
   function checkImagesLoaded() {
-    imageCount++;
-    if (imageCount === images.length) {
-      preloader.style.display = "none";
-    }
+    
+    preloader.style.display = "none"; 
+    console.log('done')
   }
 
-  images.forEach((image) => {
-    image.addEventListener("load", checkImagesLoaded);
-  });
+  setTimeout(checkImagesLoaded, 5000); 
 });
